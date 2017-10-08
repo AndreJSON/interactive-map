677065394b493b3b9c11b0782ea10e82a0364b79
AndreJSON <andre.v.json@gmail.com>
2017-10-08 17:10:19 +0200

    Update an arduino test.

--- serial_test_py/test.py
+++ serial_test_py/test.py
@@ -1,9 +1,10 @@
 import serial
 import time
 
-ser=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
-ser.baudrate=9600
+ser = serial.Serial("/dev/ttyACM0", 9600)  #change ACM number as found from ls /dev/tty/ACM*
+ser.baudrate = 9600
 
 while True:
-	read_ser=ser.readline()
-	print(read_ser)
+	#line = ser.readline()
+	line = ser.read()
+	print(ord(line))
