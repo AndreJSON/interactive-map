835ef90ab44b219053c842034ba08149d8e9d17a
AndreJSON <andre.v.json@gmail.com>
2017-10-17 17:03:18 +0200

    Add code for reading analog multiplexed buttons.

--- src/arduino/arduino.ino
+++ src/arduino/arduino.ino
@@ -1,9 +1,17 @@
 const int FAULTY_DATA = -1;
 const int START_MESSAGE = 1;
 const int END_MESSAGE = 2;
+const int BUTTON_TOLERANCE = 7;
+const int BUTTON1 = 91;
+const int BUTTON2 = 144;
+const int BUTTON3 = 203;
+const int BUTTON4 = 340;
+const int BUTTON5 = 639;
+const int BUTTON6 = 852;
 const int ledx = 13;
 const int led0 = 13;
 const int led1 = 13;
+const int buttonPin = A1;
 const int outputA = 11; //CLK of rot
 const int outputB = 12; //DT of rot
 char rotCounter = 0; 
@@ -17,6 +25,7 @@ void setup() {
   pinMode(ledx, OUTPUT);
   pinMode(led0, OUTPUT);
   pinMode(led1, OUTPUT);
+  pinMode(buttonPin, INPUT);
   pinMode (outputA,INPUT);
   pinMode (outputB,INPUT);
   aLastState = digitalRead(outputA);
@@ -57,14 +66,38 @@ void readRotary() {
       rotCounter--;
     }
     message[0] = 'R';
-    message[1] = (rotCounter + 0x30);
+    message[1] = (rotCounter + 0x30); //add 0x30 to get correct utf-8 value for digit.
     messagePos = 2; //Move forward 2 steps since we just added a 2 char message.
   } 
   aLastState = aState;
 }
 
+void readButtons() {
+  int buttonValue = analogRead(buttonPin);
+  message[0] = 'B';
+  if (buttonValue == 0) {
+    message[1] = '0';
+  } else if (buttonValue > BUTTON1 - BUTTON_TOLERANCE && buttonValue < BUTTON1 + BUTTON_TOLERANCE) {
+    message[1] = '1';
+  } else if (buttonValue > BUTTON2 - BUTTON_TOLERANCE && buttonValue < BUTTON2 + BUTTON_TOLERANCE) {
+    message[1] = '2';
+  } else if (buttonValue > BUTTON3 - BUTTON_TOLERANCE && buttonValue < BUTTON3 + BUTTON_TOLERANCE) {
+    message[1] = '3';
+  } else if (buttonValue > BUTTON4 - BUTTON_TOLERANCE && buttonValue < BUTTON4 + BUTTON_TOLERANCE) {
+    message[1] = '4';
+  } else if (buttonValue > BUTTON5 - BUTTON_TOLERANCE && buttonValue < BUTTON5 + BUTTON_TOLERANCE) {
+    message[1] = '5';
+  } else if (buttonValue > BUTTON6 - BUTTON_TOLERANCE && buttonValue < BUTTON6 + BUTTON_TOLERANCE) {
+    message[1] = '6';
+  } else { //If none of them match, we just pretend that no button was pressed.
+    message[1] = '0';
+  }
+  messagePos = 2;
+}
+
 void readSensors() {
-  readRotary();
+  //readRotary();
+  readButtons();
 }
 
 void sendMessage() {
